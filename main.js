9f3ea5f952068a39a2c02fc53a43c9f733c75847
samuel-un <samuelun00@gmail.com>
2025-02-18 09:58:28 +0100

    html and css done

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-const
+const canvas = document.getElementById("tetris");
+const ctx = canvas.getContext("2d");
+const pauseBtn = document.getElementById("pauseButton");
+const scoreElement = document.getElementById("score");
+const highScoreElement = document.getElementById("highScore");
+
